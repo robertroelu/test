7d3814bf4b92a2921f1c2d12c378bd3c0522b753
Róbert Klimant <roboklimant@gmail.com>
2024-10-12 16:04:18 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 //Finsweet attributes
-import { linkblockedit } from '@finsweet/attributes-linkblockedit/';
-import { } from '@finsweet/attributes-cmssort/'
-import { } from '@finsweet/attributes-cmsfilter/'
-import { } from '@finsweet/attributes-scrolldisable/'
-import { } from '@finsweet/attributes-cmsload/'
-import { } from '@finsweet/attributes-socialshare/'
+// import { linkblockedit } from '@finsweet/attributes-linkblockedit/';
+// import {} from '@finsweet/attributes-cmssort/';
+// import {} from '@finsweet/attributes-cmsfilter/';
+// import {} from '@finsweet/attributes-scrolldisable/';
+// import {} from '@finsweet/attributes-cmsload/';
+// import {} from '@finsweet/attributes-socialshare/';
 
 //Modal
 import { modal } from '$modal/modal';
@@ -15,16 +15,18 @@ import { nestedElement } from './nest/nestElement';
 //Utils
 import { swipers } from '$utils/swipers';
 import { consoleClear } from '$utils/consoleClear';
-import { actualYear } from '$utils/actualYear';
+// import { actualYear } from '$utils/actualYear';
 import { typer } from '$utils/typer';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  consoleClear();
-  actualYear();
-  nestedElement();
-  linkblockedit();
-  modal();
-  swipers();
-  typer();
+  // consoleClear();
+  // actualYear();
+  // nestedElement();
+  // linkblockedit();
+  // modal();
+  // swipers();
+  // typer();
+
+  console.log('ide');
 });
